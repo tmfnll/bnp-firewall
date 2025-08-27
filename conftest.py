9a76a510fb9147bfc582045631d66544e931cc98
Tom Finill <tomfinill@gmail.com>
2025-08-27 09:25:45 +0200

    Setup essential Pytest fixtures viw conftest.py

---
+++ conftest.py
@@ -0,0 +1,93 @@
+from typing import Any, Generator, cast
+
+import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.session import Session
+from sqlalchemy.orm import scoped_session
+
+from app import initialise_app
+from db import db
+from settings import Settings
+
+
+@pytest.fixture(scope="session")
+def test_db_url() -> str:
+    return "sqlite:///test_database.db"
+
+
+@pytest.fixture(scope="session")
+def settings(test_db_url: str) -> Settings:
+    return Settings(db_url=test_db_url, test=True)  # type: ignore[call-arg]
+
+
+@pytest.fixture(autouse=True, scope="session")
+def app(settings: Settings) -> Generator[Flask]:
+    app = Flask(__name__)
+
+    app.test_client_class = DefaultHeaderFlaskClient
+
+    initialise_app(app, settings)
+
+    yield app
+
+@pytest.fixture
+def app_context(app: Flask) -> Generator[Flask]:
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture
+def db_(
+    app_context: Flask,
+    settings: Settings,
+) -> Generator[SQLAlchemy]:
+    db.drop_all()
+
+    db.create_all()
+
+    yield db
+
+
+class DefaultHeaderFlaskClient(FlaskClient):
+    def __init__(
+        self, *args: Any, headers: dict[str, str] | None = None, **kwargs: Any
+    ):
+        super().__init__(*args, **kwargs)
+        self._headers = headers or {}
+
+    def open(self, *args: Any, headers: Any = None, **kwargs):
+        headers = headers or {}
+
+        headers.update(self._headers)
+
+        return super().open(*args, headers=headers, **kwargs)
+
+
+@pytest.fixture
+def auth_headers(settings: Settings) -> dict[str, str]:
+    return {"Authorization": f"Bearer {settings.api_key}"}
+
+
+@pytest.fixture
+def client(
+    app: Flask, auth_headers: dict[str, str]
+) -> DefaultHeaderFlaskClient:
+    return cast(DefaultHeaderFlaskClient, app.test_client(headers=auth_headers))
+
+
+@pytest.fixture
+def unauthenticated_client(app: Flask) -> DefaultHeaderFlaskClient:
+    return cast(DefaultHeaderFlaskClient, app.test_client())
+
+
+@pytest.fixture(autouse=True)
+def session(db_: SQLAlchemy) -> Generator[scoped_session[Session]]:
+    session = db_.session
+
+    session.begin_nested()
+
+    yield session
+
+    session.rollback()
