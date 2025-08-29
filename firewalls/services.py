900780ca522d83580cb1d6bd9e5eeb04984f9b13
Tom Finill <tomfinill@gmail.com>
2025-08-29 10:34:54 +0200

    Add a `build_firewall_rule` service.
    This allows us to easily perform actions on the whole `FirewallRule`, including relations.

---
+++ firewalls/services.py
@@ -0,0 +1,42 @@
+from typing import Sequence
+
+from db import Base
+from firewalls.models import (
+    FilteringPolicy,
+    FirewallAction,
+    FirewallRule,
+    FirewallRuleDestination,
+    FirewallRulePort,
+    FirewallRuleSource,
+    ValidationError,
+)
+
+
+def _raise_if_empty(
+    relation: Sequence[Base],
+    name: str,
+) -> None:
+    if not relation:
+        raise ValidationError(f"{name} cannot be empty")
+
+
+def build_firewall_rule(
+    filtering_policy: FilteringPolicy,
+    action: FirewallAction,
+    sources: list[FirewallRuleSource],
+    destinations: list[FirewallRuleDestination],
+    ports: list[FirewallRulePort],
+) -> FirewallRule:
+    _raise_if_empty(sources, "sources")
+    _raise_if_empty(destinations, "destinations")
+    _raise_if_empty(ports, "ports")
+
+    firewall_rule = FirewallRule(
+        filtering_policy=filtering_policy,
+        action=action,
+        sources=sources,
+        destinations=destinations,
+        ports=ports,
+    ).set_hashes()
+
+    return firewall_rule
